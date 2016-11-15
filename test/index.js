3e54b9bd3e78431afe59846dee369df5bdc01340
Yue Lv <lvyue@lechenggu.com>
2016-11-15 23:17:12 +0800

    1.模块重构

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 const Wechat = require('../lib/wechat');
 
-const wechat =  new Wechat({});
+const wechat = new Wechat({});
 
 
-wechat.handler({method:"GET",query:{"signature":"111"}},{},{});
-
+// wechat.handler({method:"GET",query:{"signature":"111"}},{},{});
+wechat.location((msg,done) => {
+    return "";
+},false).text((msg,done)=>{return ""});
+console.log(wechat.handlers);
 process.exit();
